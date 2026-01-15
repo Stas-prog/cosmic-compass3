14a2207bb2e088cb81509e4a48a393e4b5c24bdd
Stas-prog <skubav86@gmail.com>
2026-01-15 12:42:50 +0200

    11 commit

--- app/layers/ErthEquatorLayer.ts
+++ app/layers/ErthEquatorLayer.ts
@@ -1,9 +1,8 @@
 import { Vector3 } from "three";
-import { GreatCircle } from "../primitives/GreatCircle";
 
 const EARTH_AXIS = new Vector3(0, 1, 0);
 
-export function createEarthEquator(): GreatCircle {
+export function createEarthEquator() {
   return {
     normal: EARTH_AXIS.clone()
   };
